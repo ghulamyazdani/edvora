8bb2b1bef8e53f0d888ebe9bf140ce7b05a57a84
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-02-19 01:04:35 +0530

    First commit

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Image from "next/image";
+export default function header() {
+  return (
+    <div className="flex flex-row justify-between p-5 pl-8 pr-8 bg-[#101010] text-white">
+      <h1 className="text-4xl font-sf font-bold">Edmora</h1>
+      <div className="flex flex-row text-center items-center gap-3">
+        <p className="font-inter font-bold">Dhruv Singh</p>
+        <img src="./images/Rectangle.png" alt="" />
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sf': ['SF Pro Display', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
