5be216b24dce4c74972c17f8148e78843e6ea7a4
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-02-19 23:54:12 +0530

    Categories done 😉😉

--- components/List.tsx
+++ components/List.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { getUser } from "../services";
 export default function List({ Data, Up, Past }: any) {
+  var flag = 0;
   const [user, setUser]: any = useState([]);
   useEffect(() => {
     getUser().then((res) => {
@@ -29,51 +30,74 @@ export default function List({ Data, Up, Past }: any) {
     });
     return distance;
   }
-
+  function postCard(ride: any) {
+    return (
+      <div
+        key={ride.id}
+        className="flex flex-row justify-between flex-wrap pr-7 bg-[#171717] p-9 mx-10 my-6 rounded-2xl"
+      >
+        <div className=" flex flex-row gap-10 flex-wrap">
+          <img src="./images/gps.png" alt="gps" />
+          <ul className="opacity-100 ">
+            <li>
+              <span className="opacity-70">Ride id :</span> {ride.id}
+            </li>
+            <li>
+              <span className="opacity-70">Origin station :</span>{" "}
+              {ride.origin_station_code}
+            </li>
+            <li>
+              <span className="opacity-70"> Ride path :</span> [
+              {ride.station_path.toString()}]
+            </li>
+            <li>
+              <span className="opacity-70">Date :</span> {getDate(ride.date)}
+            </li>
+            <li>
+              <span className="opacity-70">Distance :</span>{" "}
+              {checkDistance(ride.station_path)}{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-row gap-5">
+          <p>
+            <span className="bg-[#0000008F] p-1 rounded-xl opacity-70">
+              {ride.city}
+            </span>
+          </p>
+          <p>
+            <span className="bg-[#0000008F] p-1 rounded-xl opacity-70">
+              {ride.state}
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+  function noRide(flag: any) {
+    if (flag === 1) {
+      return <div className="m-10 ml-12 text-xl">No rides...</div>;
+    }
+  }
   return (
     <div>
-      {Data.map((ride: any) => (
-        <div
-          key={ride.id}
-          className="flex flex-row justify-between flex-wrap pr-7 bg-[#171717] p-9 mx-10 my-6 rounded-2xl"
-        >
-          <div className=" flex flex-row gap-10 flex-wrap">
-            <img src="./images/gps.png" alt="gps" />
-            <ul className="opacity-100 ">
-              <li>
-                <span className="opacity-70">Ride id :</span> {ride.id}
-              </li>
-              <li>
-                <span className="opacity-70">Origin station :</span>{" "}
-                {ride.origin_station_code}
-              </li>
-              <li>
-                <span className="opacity-70"> Ride path :</span> [
-                {ride.station_path.toString()}]
-              </li>
-              <li>
-                <span className="opacity-70">Date :</span> {getDate(ride.date)}
-              </li>
-              <li>
-                <span className="opacity-70">Distance :</span>{" "}
-                {checkDistance(ride.station_path)}{" "}
-              </li>
-            </ul>
-          </div>
-          <div className="flex flex-row gap-5">
-            <p>
-              <span className="bg-[#0000008F] p-1 rounded-xl opacity-70">
-                {ride.city}
-              </span>
-            </p>
-            <p>
-              <span className="bg-[#0000008F] p-1 rounded-xl opacity-70">
-                {ride.state}
-              </span>
-            </p>
-          </div>
-        </div>
-      ))}
+      {Data.map((ride: any) => {
+        if (Up) {
+          if (ride.date > Date.now()) {
+            console.log(postCard(ride));
+            return postCard(ride);
+          } else {
+            flag++;
+            return noRide(flag);
+          }
+        } else if (Past) {
+          if (ride.date < Date.now()) {
+            return postCard(ride);
+          }
+        } else {
+          return postCard(ride);
+        }
+      })}
     </div>
   );
 }
